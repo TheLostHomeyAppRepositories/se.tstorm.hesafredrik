c39063a7b2ecaa97cfa711408c80b2ad50307f5a
TheodorStorm <theodor@tstorm.se>
2023-04-08 12:39:30 +0200

    Initial commit

---
+++ se.tstorm.hesafredrik/drivers/vma/device.js
@@ -0,0 +1,126 @@
+'use strict';
+
+const { Device } = require('homey');
+const axios = require('axios');
+
+class MyDevice extends Device {
+
+  testUrlRoot = 'https://vmaapi.sr.se/testapi/v2/alerts/';
+  urlRoot = 'https://vmaapi.sr.se/api/v2/alerts/';
+  incidents = this.getStoreValue('incidents') || [];
+  onoff = this.getStoreValue('onoff') || true;
+  vmaTimer = null;
+  device = null;
+
+  debug = true;
+
+  /**
+   * onInit is called when the device is initialized.
+   */
+  async onInit() {
+    this.device = this;
+    if (!this.hasCapability('message')) await this.addCapability('message');
+
+    this.log('VMA device has been initialized');
+    this.log('Name:', this.getName());
+    this.log('Id:', this.getData().id);
+
+    this.registerCapabilityListener('onoff', async (value) => {
+      this.log('onoff:', value);
+      await this.setStoreValue('onoff', value);
+    });
+
+    await this.setCapabilityValue('onoff', this.onoff);
+
+    await this.getAlerts();
+    this.vmaTimer = setInterval(async () => {
+      await this.getAlerts();
+    }, 60000);
+  }
+
+  async getAlerts() {
+    try {
+      const onoff = await this.getCapabilityValue('onoff');
+      if (!onoff) return;
+
+      let url = `${this.urlRoot}${this.getData().id}/index.json`;
+
+      if (this.debug) {
+        this.log('Debug mode is on, using test URL');
+        url = `${this.testUrlRoot}index.json`;
+      }
+      this.log(url);
+      axios.defaults.headers.common['User-Agent'] = 'Homey Hesa Fredrik (https://apps.athom.com/app/se.tstorm.hesafredrik)';
+      const response = await axios.get(url);
+      const json = response.data;
+
+      if (json.alerts) {
+        json.alerts.forEach(async (alert) => {
+          const incidentId = alert.incidents;
+          this.log(alert);
+          if (alert.msgType === 'Alert' && !this.incidents[incidentId]) {
+            this.log(`Incident ${incidentId} triggered`);
+            this.log(alert.info);
+
+            if (this.incidents[incidentId] !== alert.info) {
+              this.incidents[incidentId] = alert.info;
+              await this.setStoreValue('incidents', this.incidents);
+              await this.setCapabilityValue('message', alert.info[0].description);
+              this.driver.triggerVMA(this.device, { message: alert.info[0].description, test: alert.status === 'Test' }, {});
+            }
+          } else if (alert.msgType === 'Cancel' && this.incidents[incidentId]) {
+            this.log(`Incident ${incidentId} cancelled`);
+            delete this.incidents[incidentId];
+            await this.setStoreValue('incidents', this.incidents);
+          }
+        });
+      }
+
+      this.setCapabilityValue('alarm_generic', Object.keys(this.incidents).length > 0);
+      if (Object.keys(this.incidents).length === 0) await this.setCapabilityValue('message', null);
+
+      this.log('Incidents:', this.incidents);
+    } catch (error) {
+      this.log('Error fetching JSON:', error);
+    }
+  }
+
+  /**
+   * onAdded is called when the user adds the device, called just after pairing.
+   */
+  async onAdded() {
+    this.log('VMA device has been added');
+  }
+
+  /**
+   * onSettings is called when the user updates the device's settings.
+   * @param {object} event the onSettings event data
+   * @param {object} event.oldSettings The old settings object
+   * @param {object} event.newSettings The new settings object
+   * @param {string[]} event.changedKeys An array of keys changed since the previous version
+   * @returns {Promise<string|void>} return a custom message that will be displayed
+   */
+  async onSettings({ oldSettings, newSettings, changedKeys }) {
+    this.log('VMA device settings where changed');
+  }
+
+  /**
+   * onRenamed is called when the user updates the device's name.
+   * This method can be used this to synchronise the name to the device.
+   * @param {string} name The new name
+   */
+  async onRenamed(name) {
+    this.log('VMA device was renamed');
+  }
+
+  /**
+   * onDeleted is called when the user deleted the device.
+   */
+  async onDeleted() {
+    this.log('VMA has been deleted');
+    clearInterval(this.vmaTimer);
+  }
+
+}
+
+module.exports = MyDevice;
---
+++ se.tstorm.hesafredrik/drivers/vma/driver.js
@@ -0,0 +1,110 @@
+/* eslint-disable quote-props */
+
+'use strict';
+
+const { Driver } = require('homey');
+const { AreaCodes } = require('./areacodes');
+
+class MyDriver extends Driver {
+
+  pairingCounty = null;
+
+  /**
+   * onInit is called when the driver is initialized.
+   */
+  async onInit() {
+    this.log('VMA driver has been initialized');
+
+    this._vma_trigger = this.homey.flow.getDeviceTriggerCard('vma_trigger');
+  }
+
+  triggerVMA(device, tokens, state) {
+    this._vma_trigger
+      .trigger(device, tokens, state)
+      .then(this.log)
+      .catch(this.error);
+  }
+
+  /**
+   * onPair is called when the user starts pairing a new device
+   * @param {object} session
+   * @returns {Promise<void>}
+   */
+  async onPair(session) {
+    this.log('VMA driver onPair');
+
+    session.setHandler('get_counties', async (data) => {
+      this.log('VMA driver onPair get_counties');
+      const counties = [];
+
+      Object.keys(AreaCodes).forEach((code) => {
+        // Get code and name
+        const { name } = AreaCodes[code];
+        const county = {
+          name: `${name}`,
+          id: code,
+        };
+        counties.push(county);
+      });
+
+      return counties;
+    });
+
+    session.setHandler('set_county', async (data) => {
+      this.log('VMA driver onPair set_county');
+      this.log(data);
+      this.pairingCounty = data.id;
+    });
+
+    session.setHandler('showView', async (viewId) => {
+      this.log(`View: ${viewId}`);
+    });
+
+    session.setHandler('list_devices', async () => {
+      this.log('VMA driver onPair list_devices');
+      return this.onPairListDevices();
+    });
+  }
+
+  /**
+   * onPairListDevices is called when a user is adding a device
+   * and the 'list_devices' view is called.
+   * This should return an array with the data of devices that are available for pairing.
+   */
+  async onPairListDevices() {
+    this.log('VMA driver onPairListDevices');
+    const devices = [];
+
+    if (this.pairingCounty == null) {
+      Object.keys(AreaCodes).forEach((code) => {
+        // Get code and name
+        const { name } = AreaCodes[code];
+        const device = {
+          name: `VMA ${code} ${name}`,
+          data: {
+            id: code,
+          },
+        };
+        devices.push(device);
+      });
+    } else {
+      Object.keys(AreaCodes[this.pairingCounty]).forEach((code) => {
+        if (code === 'name') return;
+        // Get code and name
+        const name = AreaCodes[this.pairingCounty][code];
+        const device = {
+          name: `VMA ${code} ${name}`,
+          data: {
+            id: code,
+          },
+        };
+        devices.push(device);
+      });
+    }
+
+    return devices;
+  }
+
+}
+
+module.exports = MyDriver;
